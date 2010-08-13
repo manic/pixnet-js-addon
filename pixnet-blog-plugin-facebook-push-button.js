febe9c647c87db1b73c2eaaffea71c38540ef8a7
Manic Chuang <manic@pixnet.tw>
2010-08-13 18:38:05 +0800

    refactor: minimize variable

--- pixnet-blog-plugin-facebook-push-button.js
+++ pixnet-blog-plugin-facebook-push-button.js
@@ -1,10 +1,8 @@
-(function(window, document, $, undefined) {
+(function($) {
     $(function() {
 	$('div.article').each(function() {
-	    var href = jQuery(this).find('h2 a').attr('href');
-var str = '<iframe src="http://www.facebook.com/plugins/like.php?href=' + encodeURI(href) + '&amp;layout=standard&amp;show_faces=false&amp;width=600&amp;action=like&amp;font=verdana&amp;colorscheme=light&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:600px; height:35px;" allowTransparency="true"></iframe>';
-	    $(this).find('div.article-content:first').prepend(str);
+	    $(this).find('div.article-content:first').prepend('<iframe src="http://www.facebook.com/plugins/like.php?href=' + encodeURI($(this).find('h2 a').attr('href')) + '&amp;layout=standard&amp;show_faces=false&amp;width=600&amp;action=like&amp;font=verdana&amp;colorscheme=light&amp;height=35" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:600px; height:35px;" allowTransparency="true"></iframe>');
 	});
     });
-})(this, document, jQuery);
+})(jQuery);
 
