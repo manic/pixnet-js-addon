c5145c227cacfd8a73d69964da55852530d3b20d
Manic Chuang <manic.csie90@gmail.com>
2012-02-01 15:12:13 +0800

    Bug fix

--- facebook-comments/script.js
+++ facebook-comments/script.js
@@ -1,28 +1,29 @@
-(function( $, d, undefined ) {
+(function( $, d, w, undefined ) {
     $.extend({
         pixFacebookComments: function( options ) {
             var settings = $.extend({
                 num_posts: 5,
                 width: "600",
-                colorscheme: 'light',
+                colorscheme: 'light'
             }, options || {});
             // 只在單一文章頁才會作用。
-            if ( !window.location.href.match(/blog\/post/) )  return;
+            if ( !w.location.href.match(/blog\/post/) )  return;
             $(function() {
-                window.fbAsyncInit = function() {
+                w.fbAsyncInit = function() {
                     FB.init({appId: '353650924654375', status: true, cookie: true, xfbml: true});
                 };
                 $('body').append('<div id="fb-root"></div>');
                 $.getScript(d.location.protocol + '//connect.facebook.net/zh_TW/all.js');
                 var $div = $('<div class="fb-comments">');
                 $div.attr({
-                    'data-href': window.location.href,
+                    'data-href': w.location.href,
                     'data-num-posts': settings.num_posts,
                     'data-width': settings.width
                 });
                 if ('dark' == settings.colorscheme) $div.attr('data-colorscheme', 'dark');
+                $('div.article-content').append($div);
             });
         }
     });
 
-})( jQuery, document );
+})( jQuery, document, window );
