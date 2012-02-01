4f071ce00cc9f55bd751eea2eaf017f4e61a9c26
Manic Chuang <manic.csie90@gmail.com>
2012-02-01 14:25:49 +0800

    add facebook-comments

---
+++ facebook-comments/script.js
@@ -0,0 +1,28 @@
+(function( $, d, s, id, undefined ) {
+    $.extend({
+        pixFacebookComments: function( options ) {
+            var settings = $.extend({
+                num_posts: 5,
+                width: "600",
+                colorscheme: 'light',
+            }, options || {});
+            // 只在單一文章頁才會作用。
+            if ( !window.location.href.match(/blog\/post/) )  return;
+            $(function() {
+                window.fbAsyncInit = function() {
+                    FB.init({appId: '353650924654375', status: true, cookie: true, xfbml: true});
+                };
+                $('body').append('<div id="fb-root"></div>');
+                $.getScript(document.location.protocol + '//connect.facebook.net/zh_TW/all.js');
+                var $div = $('<div class="fb-comments">');
+                $div.attr({
+                    'data-href': window.location.href,
+                    'data-num-posts': settings.num_posts,
+                    'data-width': settings.width
+                });
+                if ('dark' == settings.colorscheme) $div.attr('data-colorscheme', 'dark');
+            });
+        }
+    });
+
+})( jQuery, document, 'script', 'facebook-jssdk' );
