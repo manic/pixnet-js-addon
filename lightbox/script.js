2abaf01886e822d7e4448b9d05ed71b0d3d51a77
Manic Chuang <manic@pixnet.tw>
2012-04-05 18:58:36 +0800

    更智慧的判別方法，讓 lightbox 專心對圖片做處理

--- lightbox/script.js
+++ lightbox/script.js
@@ -9,6 +9,7 @@
         imageBlank: 'http://pic.pimg.tw/appmarket/1327917749-3598307766.gif?v=1327917754',
         txtImage: '圖片',
         txtOf: '之',
+        lightboxable: "photo_page"
       }, options || {});
       var file_css = 'http://leandrovieira.com/projects/jquery/lightbox/css/jquery.lightbox-0.5.css',
           file_js = 'http://leandrovieira.com/projects/jquery/lightbox/js/jquery.lightbox-0.5.pack.js';
@@ -22,12 +23,18 @@
           return $(this).attr('src').match(/(pic|ext).pimg.tw.*\.(jpg|png|gif)/i);
         }).each(function () {
             var alink = $(this).parent(), src;
-            if (!alink.attr('href').match(/(pic|ext).pimg.tw.*\.(jpg|png|gif)/i)) {
-                src = $(this).attr('src').replace(/_[stqmnbl]/i, '_l');
-                $(this).parents('a').attr('href', src);
+            src = $(this).attr('src').replace(/_[stqmnbl]/i, '_l');
+            if (alink.attr('href').match(/\.(jpg|png|gif)/i)) { // 連結本身是原圖的，就只是加上 LightBox class
+                alink.addClass('pixLightBox');
+            } else if (settings.lightboxable == 'photo_page') {
+                if (alink.attr('href').match(/album\/photo/i)) {  // 只有連回相簿頁的圖片才加連結
+                  alink.attr('href', src).addClass('pixLightBox');
+                }
+            } else {
+              alink.attr('href', src).addClass('pixLightBox');
             }
         });
-        $('div.article-content').find('a').has('img').lightBox(settings);
+        $('div.article-content').find('a.pixLightBox').has('img').lightBox(settings);
       });
     }
   });
